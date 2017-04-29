eda51c193f705bd772973b608b365d121ab03ae6
Shakeel Saleemi <shakahs@gmail.com>
2017-04-28 17:47:38 -0700

    Update schema.sql file with new schema from database modeling tool

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
 CREATE TABLE "Users" (
-	"ID" serial NOT NULL UNIQUE,
+	"id" serial NOT NULL UNIQUE,
 	"email" VARCHAR(255) NOT NULL UNIQUE,
-	"Name" VARCHAR(255) NOT NULL,
-	"Location" VARCHAR(255) NOT NULL,
-	CONSTRAINT Users_pk PRIMARY KEY ("ID")
+	"name" VARCHAR(255) NOT NULL,
+	"address" VARCHAR(255) NOT NULL,
+	"serviceId" integer NOT NULL,
+	CONSTRAINT Users_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -11,21 +12,9 @@ CREATE TABLE "Users" (
 
 
 CREATE TABLE "Services" (
-	"ID" serial NOT NULL UNIQUE,
-	"Type" VARCHAR(255) NOT NULL UNIQUE,
-	CONSTRAINT Services_pk PRIMARY KEY ("ID")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "User_Service_Mapping" (
-	"ID" serial NOT NULL UNIQUE,
-	"User" integer NOT NULL,
-	"Service" integer NOT NULL,
-	"expectation" TEXT NOT NULL,
-	CONSTRAINT User_Service_Mapping_pk PRIMARY KEY ("ID")
+	"id" serial NOT NULL UNIQUE,
+	"type" VARCHAR(255) NOT NULL UNIQUE,
+	CONSTRAINT Services_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -33,11 +22,11 @@ CREATE TABLE "User_Service_Mapping" (
 
 
 CREATE TABLE "Engagements" (
-	"ID" serial NOT NULL UNIQUE,
-	"customer" integer NOT NULL,
-	"provider" integer NOT NULL,
+	"id" serial NOT NULL UNIQUE,
+	"customerId" integer NOT NULL,
+	"providerId" integer NOT NULL,
 	"complete" BOOLEAN NOT NULL,
-	CONSTRAINT Engagements_pk PRIMARY KEY ("ID")
+	CONSTRAINT Engagements_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -45,12 +34,12 @@ CREATE TABLE "Engagements" (
 
 
 CREATE TABLE "Messages" (
-	"ID" serial NOT NULL UNIQUE,
-	"engagement" integer NOT NULL,
-	"from" integer NOT NULL,
-	"to" integer NOT NULL,
-	"message" VARCHAR(255) NOT NULL,
-	CONSTRAINT Messages_pk PRIMARY KEY ("ID")
+	"id" serial NOT NULL UNIQUE,
+	"engagementId" integer NOT NULL,
+	"fromId" integer NOT NULL,
+	"toId" integer NOT NULL,
+	"message" TEXT NOT NULL,
+	CONSTRAINT Messages_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -58,30 +47,27 @@ CREATE TABLE "Messages" (
 
 
 CREATE TABLE "Service Review" (
-	"ID" serial NOT NULL UNIQUE,
-	"user" integer NOT NULL,
-	"service" integer NOT NULL,
+	"id" serial NOT NULL UNIQUE,
+	"userId" integer NOT NULL,
+	"engagementId" integer NOT NULL,
 	"score" integer NOT NULL,
-	"text" TEXT NOT NULL,
-	CONSTRAINT Service Review_pk PRIMARY KEY ("ID")
+	"review" TEXT NOT NULL,
+	CONSTRAINT Service Review_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
+ALTER TABLE "Users" ADD CONSTRAINT "Users_fk0" FOREIGN KEY ("serviceId") REFERENCES "Services"("id");
 
 
-ALTER TABLE "User_Service_Mapping" ADD CONSTRAINT "User_Service_Mapping_fk0" FOREIGN KEY ("User") REFERENCES "Users"("ID");
-ALTER TABLE "User_Service_Mapping" ADD CONSTRAINT "User_Service_Mapping_fk1" FOREIGN KEY ("Service") REFERENCES "Services"("ID");
+ALTER TABLE "Engagements" ADD CONSTRAINT "Engagements_fk0" FOREIGN KEY ("customerId") REFERENCES "Users"("id");
+ALTER TABLE "Engagements" ADD CONSTRAINT "Engagements_fk1" FOREIGN KEY ("providerId") REFERENCES "Users"("id");
 
-ALTER TABLE "Engagements" ADD CONSTRAINT "Engagements_fk0" FOREIGN KEY ("customer") REFERENCES "Users"("ID");
-ALTER TABLE "Engagements" ADD CONSTRAINT "Engagements_fk1" FOREIGN KEY ("provider") REFERENCES "Users"("ID");
+ALTER TABLE "Messages" ADD CONSTRAINT "Messages_fk0" FOREIGN KEY ("engagementId") REFERENCES "Engagements"("id");
+ALTER TABLE "Messages" ADD CONSTRAINT "Messages_fk1" FOREIGN KEY ("fromId") REFERENCES "Users"("id");
+ALTER TABLE "Messages" ADD CONSTRAINT "Messages_fk2" FOREIGN KEY ("toId") REFERENCES "Users"("id");
 
-ALTER TABLE "Messages" ADD CONSTRAINT "Messages_fk0" FOREIGN KEY ("engagement") REFERENCES "Engagements"("ID");
-ALTER TABLE "Messages" ADD CONSTRAINT "Messages_fk1" FOREIGN KEY ("from") REFERENCES "Users"("ID");
-ALTER TABLE "Messages" ADD CONSTRAINT "Messages_fk2" FOREIGN KEY ("to") REFERENCES "Users"("ID");
-
-ALTER TABLE "Service Review" ADD CONSTRAINT "Service Review_fk0" FOREIGN KEY ("user") REFERENCES "Users"("ID");
-ALTER TABLE "Service Review" ADD CONSTRAINT "Service Review_fk1" FOREIGN KEY ("service") REFERENCES "User_Service_Mapping"("ID");
-
+ALTER TABLE "Service Review" ADD CONSTRAINT "Service Review_fk0" FOREIGN KEY ("userId") REFERENCES "Users"("id");
+ALTER TABLE "Service Review" ADD CONSTRAINT "Service Review_fk1" FOREIGN KEY ("engagementId") REFERENCES "Engagements"("id");
