db3f8228f32af581a3e8f706f004876f34d46114
Yubin Kim <joeekimm@gmail.com>
2017-05-01 23:36:33 -0700

    removed defaultState

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,6 +3,6 @@ import { routerReducer } from 'react-router-redux';
 
 import ProfileReducer from './profileReducer';
 
-const rootReducer = combineReducers({ProfileReducer, routing: routerReducer});
+const rootReducer = combineReducers({profile: ProfileReducer, routing: routerReducer});
 
 export default rootReducer;
