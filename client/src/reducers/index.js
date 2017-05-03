ef45fdea0ea7da3a2818320e5c5495861aa6db8d
Yubin Kim <joeekimm@gmail.com>
2017-05-02 20:32:29 -0700

    part of auth0 functionality

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import { routerReducer as routing } from 'react-router-redux';
 import ProfileReducer from './profileReducer';
+import auth from './auth';
 
-const rootReducer = combineReducers({ProfileReducer, routing: routerReducer});
+const rootReducer = combineReducers({
+  routing,
+  ProfileReducer,
+  auth
+});
 
 export default rootReducer;
