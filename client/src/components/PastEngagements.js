e97572dc235f97c8596b793c2d15ab929ebf60b3
Justin Kang <JMKANG06@gmail.com>
2017-05-03 19:05:24 -0700

    Added Past engagements component

--- client/src/components/PastEngagements.js
+++ client/src/components/PastEngagements.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import axios from 'axios';
+import _ from 'lodash';
+import PastEngagementsList from './PastEngagementsList';
 
 class PastEngagements extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      pastEngagements: [],
+      messages: []
+    }
+    this.fetchPast = this.fetchPast.bind(this);
+  }
+  
+  componentDidMount() {
+    this.fetchPast();
+  }
+
+  fetchPast() {
+    const config = {
+      headers: {'Authorization': 'Bearer ' + token}
+    };
+    axios.get('/engagements?completed=true', config)
+         .then(data => {
+           console.log(data);
+           _.each(data, datum => {
+            this.setState({pastEngagements:[datum, ...this.state.pastEngagements]})
+           })
+         })
+         .catch(err => {
+           console.log("Error in fetchPast: " , err)          
+         })
+  }
+
   render() {
     return(
       <div>
-        Past Engagements!
+        <PastEngagementsList engagements={this.state.engagements}/>
       </div>
     )
   }
