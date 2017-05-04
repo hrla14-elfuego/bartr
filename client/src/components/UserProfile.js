a164d0815a9048b63961369757201a19334437f2
Yubin Kim <joeekimm@gmail.com>
2017-05-04 00:59:42 -0700

    changed users profile picture to large picture

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -6,8 +6,8 @@ const UserProfile = ({ profile, error }) => {
   console.log('this is userprofile PROFILE: ', profile)
   return(
     <div>
-      <h1 style={{textAlign: 'center', fontSize: '30px'}}>{profile.name ? 'Hello, ' + profile.name : null}</h1>
-      <img src={profile.picture} height="50px" />
+      <h1 style={{textAlign: 'center', fontSize: '50px'}}>{profile.name ? 'Hello, ' + profile.name : null}</h1>
+      <img src={profile.picture_large} height="50px" />
       <br/>
       <h1 style={{}}><Link to='editprofile'><Button>Edit Profile</Button></Link></h1>
       <Link to='pastengagements'><Button>Past Engagements</Button></Link>
