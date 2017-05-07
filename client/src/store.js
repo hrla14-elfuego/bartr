04cacd9ae49bc9c825adbd0f93e18636d7dc0986
Shakeel Saleemi <shakahs@gmail.com>
2017-05-06 19:12:02 -0700

    Implement changes for Redux chrome extension to work

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/index';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 function createStoreWithMiddleware (prevState) {
   const middleware = [thunk, logger];
-  return createStore(
-    rootReducer,
-    prevState,
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(rootReducer, prevState, composeEnhancers(
     applyMiddleware(...middleware)
-  )
+  ));
 };
 
 export default createStoreWithMiddleware;
