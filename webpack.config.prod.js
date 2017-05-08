fb4163d9a361067f44ba70d332a238108583a559
Shakeel Saleemi <shakahs@gmail.com>
2017-05-08 12:00:05 -0700

    Add env variable processing back to webpack prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -39,4 +39,12 @@ module.exports = {
     }
     ]
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'API_ENDPOINT': JSON.stringify(""),
+      'AUTH0_CLIENT_ID': JSON.stringify(process.env.AUTH0_CLIENT_ID),
+      'AUTH0_DOMAIN': JSON.stringify(process.env.AUTH0_DOMAIN),
+    })
+  ]
 };
